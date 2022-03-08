9c4c1a635cbb69e0ad33455a61bf8fefb5aac750
Anatolii Dergachev <anatolka18@yandex.ru>
2022-03-08 23:52:13 +0400

    programm fib

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-cout << " Hello world!" << endl;
-return 0;
+int fib(int n){
+  if (n == 1 || n == 2)
+    return 1;
+  return fib(n - 1) + fib(n - 2);
 }
+
+int main(){
+  int a = 6;
+  cout << fib(a) << endl;
+  return 0;
+}
+
